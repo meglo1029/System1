74bd7fe6a60c3aea42149683352685b45d5ab755
Meghan Logan <meglo1029@gmail.com>
2017-09-28 18:29:09 -0700

    all commits for travelville

---
+++ main.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closeNav() {
+   document.getElementById("overlay").style.display = "none";
+   // document.getElementByClassName("hideLogo").style.display = "none";
+}
